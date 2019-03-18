c4e61bca31a7527879022c334c9dcc873c92dd07
ShikangJin <shikang.jin@sjsu.edu>
2019-03-18 15:47:50 -0700

    clean project structure

--- RecommendationApp/src/main/java/skjinnero/com/recommendation/RecommendationApplication.java
+++ RecommendationApp/src/main/java/skjinnero/com/recommendation/RecommendationApplication.java
@@ -2,14 +2,6 @@ package skjinnero.com.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class RecommendationApplication {
